d14cef862b90accb16d8ed1d3c2ba3d4692c6053
elyonousimohamed <melyonousi@redimpactarabia.com>
2023-09-11 22:54:42 +0100

    update download video and playlist

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,15 @@
 const express = require('express');
+const { format } = require('morgan');
 const router = express.Router();
 const ytdl = require('ytdl-core');
 const ytpl = require('ytpl');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
     res.render('index', { title: 'YTBDN (Youtube Downloader Video)' });
 });
 
-router.get('/search', async(req, res) => {
+router.get('/search', async (req, res) => {
     try {
         const url = ytdl.getURLVideoID(req.query.url)
         const info = await ytdl.getInfo(url);
@@ -23,6 +24,7 @@ router.get('/search', async(req, res) => {
         let youtubeAudio = []
         arrayAudio.forEach(item => {
             youtubeAudio.push({
+                link: req.query.url,
                 url: item.url,
                 mimeType: item.mimeType.split(';')[0],
                 audioQuality: item.audioQuality
@@ -54,28 +56,37 @@ router.get('/search', async(req, res) => {
     }
 });
 
-router.get('/playlist', async(req, res) => {
+router.get('/playlist', async (req, res) => {
     res.render('playlist', { title: 'YTBDN (Youtube Downloader Playlist)' })
 })
 
-router.get('/searchplaylist', async(req, res) => {
+router.get('/searchplaylist', async (req, res) => {
     try {
         const urlytpl = (req.query.url).split("list=")[1].split("&")[0]
-        if (await ytpl.validateID(urlytpl)) {
+        if (ytpl.validateID(urlytpl)) {
             const playlist = await ytpl(urlytpl, { pages: 50 });
             let youtubeplaylist = []
-            playlist.items.forEach(item => {
+            for (const item of playlist.items) {
+                const qualityLabels = (await ytdl.getInfo(item.id)).player_response.streamingData.formats
                 youtubeplaylist.push({
-                    image: item.bestThumbnail.url,
                     title: item.title,
+                    image: item.bestThumbnail.url,
                     index: item.index,
-                    id: item.id,
                     duration: item.duration,
+                    id: item.id,
                     url: item.shortUrl,
-                    titleLength: item.title.length
+                    titleLength: item.title.length,
+                    qualityLabels: qualityLabels
                 })
-            });
-            res.render('playlist', { title: `YTBDN (${playlist.title} '${playlist.estimatedItemCount}')`, count: playlist.estimatedItemCount, playlistUrl: playlist.url, youtube: youtubeplaylist })
+            };
+
+            res.render('playlist',
+                {
+                    title: `YTBDN ${playlist.title} (${playlist.estimatedItemCount})`,
+                    count: playlist.estimatedItemCount,
+                    playlistUrl: playlist.url,
+                    youtube: youtubeplaylist
+                })
         } else {
             res.render('playlist', { title: 'YTBDN (playlist not found)' })
         }
@@ -84,15 +95,52 @@ router.get('/searchplaylist', async(req, res) => {
     }
 })
 
-router.get('/download', async(req, res) => {
+router.get('/download-video', async (req, res) => {
+    try {
+        const videoUrl = req.query.link;
+        const qualityLabel = req.query.qualityLabel;
+        const videoId = ytdl.getURLVideoID(videoUrl);
+        const info = await ytdl.getInfo(videoId);
+        const videoTitle = info.videoDetails.title;
 
-    const infos = {
-        link: req.query.link,
+        res.setHeader('Content-Disposition', `attachment; filename="${encodeURI(videoTitle)}.mp4"`);
+
+        const format = ytdl.chooseFormat(info.formats,
+            {
+                quality: 'highest',
+                filter: format => format.qualityLabel === qualityLabel &&
+                    format.hasAudio &&
+                    format.hasAudio
+            });
+        ytdl(videoUrl, { format: format }).pipe(res);
+
+    } catch (error) {
+        res.render('index', { title: 'Failed to fetch video info or the video is not available.' });
     }
-    res.header("Content-Disposition", 'attachment;\  filename="' + ytdl.getURLVideoID(infos.link) + '.mp4"');
-    const info = await ytdl.getInfo(infos.link);
-    const format = ytdl.chooseFormat(info.formats, { quality: 'highest' });
-    ytdl(infos.link, { format: format }).pipe(res);
+});
+
+router.get('/download-audio', async (req, res) => {
+    try {
+        const videoUrl = req.query.link
+        const audioQuality = req.query.audioQuality
+        const videoId = ytdl.getVideoID(videoUrl);
+        const data = await ytdl.getInfo(videoId);
+        const videoTitle = data.videoDetails.title;
+
+        const audioFormats = ytdl.filterFormats(data.formats, 'audioonly');
+
+        const audioFormat = ytdl.chooseFormat(audioFormats, {
+            quality: 'highestaudio',
+            filter: (format) => format.hasAudio && format.audioQuality === audioQuality,
+        });
+
+        res.setHeader('Content-Disposition', `attachment; filename="${encodeURI(videoTitle)}.mp3"`);
+        res.setHeader('Content-Type', 'audio/mp3');
+
+        ytdl(videoUrl, { format: audioFormat }).pipe(res);
+    } catch (error) {
+        res.render('index', { title: 'Failed to fetch aduio info or the aduio is not available.' });
+    }
 });
 
 module.exports = router;
